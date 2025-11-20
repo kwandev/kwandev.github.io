ce5560141d755a32b4e570c174cfb72636ef9473
이경환 <rudghks7816@gmail.com>
2025-11-20 09:38:06 +0900

    🔧 (chore): trailingSlash 옵션 변경
    - 사용안하도록 'never'로 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kwandev.github.io",
+  trailingSlash: "never",
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
