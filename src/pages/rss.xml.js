06aa430c14bedccbbde1b5b2da851806dbc84c71
kwandev <rudghks7816@gmail.com>
2025-12-12 12:47:25 +0900

    ♻️ (refactor): rss.xml 파일 수정

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -17,7 +17,7 @@ export async function GET(context) {
       title: item.data.title,
       description: item.data.description,
       pubDate: item.data.date,
-      link: `/${item.collection}/${item.id}/`,
+      link: `/${item.collection}/${item.id}`,
     })),
   });
 }
