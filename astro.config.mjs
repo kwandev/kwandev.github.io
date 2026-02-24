772c43e61e6f9e6be4de5e3016700e6393faa5b5
kwandev <rudghks7816@gmail.com>
2026-02-24 16:19:08 +0900

    🔧 (chore): sitemap 설정 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kwandev.github.io",
-  trailingSlash: "never",
-  build: {
-    format: 'file'
-  },
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [
+    sitemap({
+      filter: (page) => !page.startsWith("https://kwandev.github.io/post/"),
+    }),
+    mdx(),
+    pagefind(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
